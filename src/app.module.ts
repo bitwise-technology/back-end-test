3645f1b6a97e36d580de65c4afd38b52f51bad33
Hitallo William <hitallo@viggosistemas.com.br>
2020-12-02 01:59:58 -0300

    feat: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
+import { UserModule } from './modules/user/user.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      logging: true,
+      synchronize: true,
+      database: 'data/dev-db.sqlite',
+      entities: ['dist/**/*.model{.ts,.js}']
+    }),
+    UserModule
+  ],
   controllers: [AppController]
 })
 export class AppModule {}
