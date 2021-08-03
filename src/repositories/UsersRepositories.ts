a9bf2338ed2d435a444308627052c649dd5c2fef
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-08-03 13:32:00 -0300

    Criando Repositorio de usuário

---
+++ src/repositories/UsersRepositories.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import User from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepositories extends Repository<User> {}
+
+export { UsersRepositories };
