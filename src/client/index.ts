0445bada9e40ab5054425f30cce993e332cbb059
José Silva <jcnascimento1994@gmail.com>
2021-08-03 22:02:24 -0300

    Adicionada funçao de acesso ao API do github

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,19 +1,39 @@
-import { request, GraphQLClient, gql } from 'graphql-request';
+import { GraphQLClient, gql } from 'graphql-request';
 
 import githubApi from './github-api.json';
+import GithubUser from './model/GithubUser';
 
+// cliente usado para fazer requisições HTTP
 const client = new GraphQLClient(githubApi.uri, {
   headers: {
     Authorization: `bearer ${githubApi.token}`
   }
 });
 
-const testQuery = gql`
-  {
-    user(login: "josecarlosns") {
+// Query de consulta das informações de usuário
+const query = gql`
+  query getUser($login: String!) {
+    user(login: $login) {
+      avatarUrl
+      bio
       email
+      name
+      login
     }
   }
 `;
 
-client.request(testQuery).then(data => console.log(data));
+// função assíncrona de acesso à API do github
+async function getUser(username: string): Promise<GithubUser> {
+  try {
+    const data = await client.request<GithubUser>(query, {
+      login: username
+    });
+
+    return data;
+  } catch (e) {
+    throw new Error('user not found');
+  }
+}
+
+export default getUser;
---
+++ src/client/model/GithubUser.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface de dados da API do github
+ */
+export default interface GithubUser {
+  avatarUrl: string;
+
+  bio: string;
+
+  email: string;
+
+  name: string;
+
+  userName: string;
+}
