f80e93fd863126230f2773997cd02118facd2a8c
José Silva <jcnascimento1994@gmail.com>
2021-08-04 14:27:13 -0300

    Documentaçao

--- src/client/model/GithubUser.ts
+++ src/client/model/GithubUser.ts
@@ -1,5 +1,9 @@
 /**
  * Interface de dados da API do github
+ * 
+ * FIXME github NÃO contém informações de gênero!
+ * portanto não é possível recuperar o gênero de um usuário
+ * do github
  */
 export default interface GithubUser {
   avatarUrl: string;
