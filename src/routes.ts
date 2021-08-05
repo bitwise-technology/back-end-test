f2ed9a3552faa9f482578b2156976862d0d8c69b
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-08-05 14:27:11 -0300

    Adicionando alterar usuario a rota

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import CreateUserController from './controllers/CreateUserController';
 import CreateUserGithubController from './controllers/CreateUserGithubController';
 import ListUserController from './controllers/ListUserController';
+import AlterUserController from './controllers/AlterUserController';
 
 const router = Router();
 
@@ -11,8 +12,11 @@ const createUserGithubController = new CreateUserGithubController();
 
 const listUserController = new ListUserController();
 
+const alterUserController = new AlterUserController();
+
 router.post('/users', createUserController.handle);
 router.post('/githubusers', createUserGithubController.handle);
 router.get('/user', listUserController.handle);
+router.post('/updateuser', alterUserController.handle);
 
 export { router };
