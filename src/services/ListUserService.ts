80fc28217eb63798db41f1b21c05640abcc88002
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-08-05 10:31:15 -0300

    Adicionando verificação de campos de dados

--- src/services/ListUserService.ts
+++ src/services/ListUserService.ts
@@ -5,6 +5,10 @@ import UsersRepositories from '../repositories/UsersRepositories';
 
 class ListUserService {
   async execute(username: string, email: string): Promise<User | undefined> {
+    if (!username && !email) {
+      throw new Error('Missing required field: username or email');
+    }
+
     const usersRepositories = getCustomRepository(UsersRepositories);
 
     const user = await usersRepositories.findOne({
