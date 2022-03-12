e86f3dc435003b2c6e266d7e4b97d0176a627f05
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-03-12 19:46:12 -0300

    Atualização

--- Verificar_usuarios_entidades.py
+++ Verificar_usuarios_entidades.py
@@ -1,4 +1,5 @@
 import requests
+import Banco_de_dados
 def validar_usuario_git(user):
     try:
         GitHub = requests.get("https://api.github.com/users/" + user)
@@ -7,17 +8,20 @@ def validar_usuario_git(user):
         print(validado)
         print("Usuário validado!")
         return True
+
+
+
     except:
        return False
 
-def validar_entidades(palavra,enti,menor, maior, alfanumerico_ou_letras):
+def validar_entidades(palavra,menor, maior, alfanumerico_ou_letras):
     tamanho = len(palavra)
     pala =  palavra
     lista = list(pala)
 
     if tamanho >= menor and tamanho <= maior:
         if alfanumerico_ou_letras == 1:
-            return True;
+            return True
         elif alfanumerico_ou_letras == 2:
             for i in range(len(lista)):
                 if lista[i]=='0' or lista[i]=='1' or lista[i]=='2' or lista[i]=='3' or lista[i]=='4' or lista[i]=='5' or lista[i]=='6' or lista[i]=='7' or lista[i]=='8' or lista[i]=='9':
