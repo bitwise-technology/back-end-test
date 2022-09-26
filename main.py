57064900d19a299cead9ee84fc5c3e70523ddad7
Cristovão <cristovao.s.lima@gmail.com>
2022-09-26 16:11:18 -0300

    documentação

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from core.configs import settings
 from api.v1.api import api_router
 
-app: FastAPI = FastAPI(title='Teste Back-end - FastAPI')
+app: FastAPI = FastAPI(
+    title='Teste Back-end - FastAPI',
+    version='0.0.1',
+    description='Uma api de teste para desenvolvedor back-end ultilizando FastAPI.'
+)
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == '__main__':
