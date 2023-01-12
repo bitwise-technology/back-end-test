87eb7666d77daaa235b696e71af8dde2be1f7f69
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2023-01-12 09:57:17 -0300

    Revisão final do código

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,7 +6,7 @@ import { userSchema } from "../validations/UserSchema";
 const routes = Router();
 
 routes.post('/user', schemaValidation(userSchema), new UserController().create);
-routes.post('/userGithub', new UserController().createUserGithub);
+routes.post('/user-github', new UserController().createUserGithub);
 routes.put('/user/:id', schemaValidation(userSchema), new UserController().update);
 routes.get('/user/username/:username', new UserController().readUsername);
 routes.get('/user/email/:email', new UserController().readEmail);
