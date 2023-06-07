5218ea755abe16b501075d3a133b2f2a3bb0d849
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 08:49:34 -0300

    refactor: escondendo secret key

--- bitwise_teste/settings.py
+++ bitwise_teste/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3qvygpvp2r5(7tn*dn)9lmw9k87_sk5t@1n(v&a4usvspqaklq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
