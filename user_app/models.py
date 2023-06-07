5a489a6be7cf000bb753fe83930c8bc000176dce
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 10:41:11 -0300

    refactor: alterando campo gender

--- user_app/models.py
+++ user_app/models.py
@@ -3,10 +3,11 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 GENDER_CHOICES = (
-    ('M', 'Masculino'),
-    ('F', 'Feminino'),
-    ('N', 'Não Especificado'),
+    ('Male', 'Male'),
+    ('Female ', 'Female'),
+    ('Not Specified', '')
 )
+
 
 class User(models.Model):
     username = models.CharField(max_length=30,unique=True, validators=[MinLengthValidator(5)])
@@ -15,8 +16,7 @@ class User(models.Model):
     profile_image_url = models.ImageField(upload_to='profile_images', null=True, blank=True)
     bio = models.CharField(max_length=30, validators=[MinLengthValidator(3)], null=True, blank=True)
     email = models.EmailField(unique=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-
+    gender = models.CharField(max_length=13, choices=GENDER_CHOICES, blank=True, null=True, default=None)
 
     class Meta:
         ordering = ('username',)
@@ -25,3 +25,8 @@ class User(models.Model):
 
     def __str__(self):
         return f'{self.username}'
+    
+    def save(self,*args, **kwargs):
+        if self.gender is None:
+            self.gender = "Not Specified"
+        super().save(*args, **kwargs)
