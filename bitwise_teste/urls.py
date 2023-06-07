884ecb93eebee1d1d54fab88d3f46297ead09517
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 10:40:37 -0300

    feat: descomentando url

--- bitwise_teste/urls.py
+++ bitwise_teste/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/v1/', include('user_app.urls')),
+    path('api/v1/', include('user_app.urls')),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
