cb584f8a41f906420ef9b9661818efb6657a2e56
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 10:39:42 -0300

    feat:criando view UserCreateView

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
