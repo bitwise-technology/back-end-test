7f9b916fa82de202a1d57f0ec576895980789ac2
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-08 18:50:33 -0300

    feat: add rotas do swagger

--- bitwise_teste/urls.py
+++ bitwise_teste/urls.py
@@ -17,8 +17,28 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+            title="Bitwise Test",
+            default_version='v1',
+            description="Test description",
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="contact@snippets.local"),
+            license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('user_app.urls')),
+    path('api/', include('user_app.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
