1eb9cf2307b8e004f73b9850e208a922c2f99972
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 21:05:39 -0300

    refactor: alterando field de imagem e gender

--- user_app/models.py
+++ user_app/models.py
@@ -4,8 +4,7 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 GENDER_CHOICES = (
     ('Male', 'Male'),
-    ('Female ', 'Female'),
-    ('Not Specified', '')
+    ('Female', 'Female'),
 )
 
 
@@ -13,10 +12,10 @@ class User(models.Model):
     username = models.CharField(max_length=30,unique=True, validators=[MinLengthValidator(5)])
     name = models.CharField(max_length=30,  validators=[MinLengthValidator(3)])
     last_name = models.CharField(max_length=30, validators=[MinLengthValidator(3)], null=True, blank=True)
-    profile_image_url = models.ImageField(upload_to='profile_images', null=True, blank=True)
+    profile_image_url = models.CharField(max_length=255, null=True, blank=True)
     bio = models.CharField(max_length=30, validators=[MinLengthValidator(3)], null=True, blank=True)
     email = models.EmailField(unique=True)
-    gender = models.CharField(max_length=13, choices=GENDER_CHOICES, blank=True, null=True, default=None)
+    gender = models.CharField(max_length=13, choices=GENDER_CHOICES, blank=True, null=True)
 
     class Meta:
         ordering = ('username',)
