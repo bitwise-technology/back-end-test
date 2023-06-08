2209b2ee64657322a04afc9772b612623ce6c87b
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 22:03:17 -0300

    feat: adicionando rotas de username e email

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("user/create/", UserCreateView.as_view(), name="user_create_view"),
     path("user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
+    path("user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
+    path("user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
 ]
