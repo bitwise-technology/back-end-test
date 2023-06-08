ad1f6bcde480b820ff8cd7e260f133c3b28dc606
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-08 16:26:05 -0300

    fix: removendo rota duplicada

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path("user/create/", UserCreateView.as_view(), name="user_create_view"),
-    path("user/create/github/", UserCreateViewFromGithub.as_view(), name="user_create_github_view"),
     path("user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
     path("user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
     path("user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
