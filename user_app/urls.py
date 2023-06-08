7e737df71f9c04bb03d8821341b9f17d5eea28fa
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-08 12:08:31 -0300

    feat: add rotas do user github

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,15 @@
-from .views import UserCreateView, UserUpdateView, UserDetailByEmailView, UserDetailByUsernameView
+from user_app.views.user_views import *
+from user_app.views.user_github_views import *
 from django.urls import path
 
 urlpatterns = [
     path("user/create/", UserCreateView.as_view(), name="user_create_view"),
+    path("user/create/github/", UserCreateViewFromGithub.as_view(), name="user_create_github_view"),
     path("user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
     path("user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
     path("user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
+
+    path("user/github/create/", UserCreateViewFromGithub.as_view(), name="user_github_create_view"),
+    path("user/github/login/<str:login>/", UserGithubDetailByUsernameView.as_view(), name="user_detail_username_view"),
+
 ]
