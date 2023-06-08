9b4c9ed7e91deea2f07c3241aa4c11417ce2848a
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-07 21:05:05 -0300

    feat: exibindo mensagem de sucesso

--- user_app/views.py
+++ user_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import UserSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        response_data = {
+            'message': 'User saved successfully',
+            'user': serializer.data
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
