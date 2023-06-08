0a3097c995e51d2a9d7d1e5cb1a1000510e20147
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-08 18:52:35 -0300

    fix: corrigindo rotas

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,13 +5,13 @@ from django.urls import path
 app_name = "user"
 
 urlpatterns = [
-    path("v1//user/create/", UserCreateView.as_view(), name="user_create_view"),
-    path("v1//user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
-    path("v1//user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
-    path("v1//user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
+    path("v1/user/create/", UserCreateView.as_view(), name="user_create_view"),
+    path("v1/user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
+    path("v1/user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
+    path("v1/user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
 
-    path("v1//github_user/create/", UserCreateViewFromGithub.as_view(), name="user_github_create_view"),
-    path("v1//github_user/login/<str:login>/", UserGithubDetailByUsernameView.as_view(), name="user_detail_username_view"),
-    path("v1//github_user/update/<int:id>/", UserGithubUpdateView.as_view(), name="user_update_view"),
+    path("v1/github_user/create/", UserCreateViewFromGithub.as_view(), name="user_github_create_view"),
+    path("v1/github_user/login/<str:login>/", UserGithubDetailByUsernameView.as_view(), name="user_detail_username_view"),
+    path("v1/github_user/update/<int:id>/", UserGithubUpdateView.as_view(), name="user_update_view"),
 
 ]
