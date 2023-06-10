5d349bdc850a6a92617e5602c04bfee66963e501
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-10 11:08:50 -0300

    feat: add rota de busca em user

--- user_app/urls.py
+++ user_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("v1/user/update/<int:id>/", UserUpdateView.as_view(), name="user_update_view"),
     path("v1/user/detail/username/<str:username>/", UserDetailByUsernameView.as_view(), name="user_detail_username_view"),
     path("v1/user/detail/email/<str:email>/", UserDetailByEmailView.as_view(), name="user_detail_email_view"),
+    path("v1/user/search/<str:username>/", UserSearchView.as_view(), name="user_search_view"),
 
     path("v1/github_user/create/", UserCreateViewFromGithub.as_view(), name="user_github_create_view"),
     path("v1/github_user/login/<str:login>/", UserGithubDetailByUsernameView.as_view(), name="user_github_detail_username_view"),
