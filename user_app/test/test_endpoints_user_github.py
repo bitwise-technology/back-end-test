4e66fe7a571d8200be9e6229c7e95ceca3d705ee
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-06-10 11:09:26 -0300

    test: add teste a busca de user github

--- user_app/test/test_endpoints_user_github.py
+++ user_app/test/test_endpoints_user_github.py
@@ -2,10 +2,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from user_app.models import UserGithub 
-from user_app.serializers import UserSerializer
 from rest_framework.test import APIRequestFactory
-from user_app.views.user_github_views import UserCreateViewFromGithub
-from unittest import mock
+from user_app.serializers import UserGithubViewSerializer
 
 class UserCreateViewFromGithubTest(APITestCase):
     def setUp(self):
@@ -23,15 +21,6 @@ class UserCreateViewFromGithubTest(APITestCase):
             followers = 10,
             following = 10
         )
-    # def test_success_create_user_with_github(self):
-        
-    #     url = reverse("user:user_github_create_view")
-    #     data = {"login": "username_teste"}
-
-    #     response = self.client.post(url, data, format='json')
-
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(response.data['message'], 'User saved successfully')
 
     def test_get_user_github_by_username(self):
 
@@ -89,3 +78,19 @@ class UserCreateViewFromGithubTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('login', response.data)
+
+
+    def test_search_users_by_username(self):
+        url = reverse("user:user_github_search_view", kwargs={'login': 'username_teste'})
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        expected_data = UserGithubViewSerializer([self.user], many=True).data
+        self.assertEqual(response.data['results'], expected_data)
+
+    def test_search_users_by_username_with_no_results(self):
+        url = reverse("user:user_github_search_view", kwargs={'login': 'non_existent_username'})
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
