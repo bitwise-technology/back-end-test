17e956514c68ec4fdf58ebf93b1c749f4c29cbe5
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-13 11:21:18 -0300

    feat(user.model): creating user interface and inputs dto

---
+++ src/models/user.model.ts
@@ -0,0 +1,15 @@
+import { Gender, Prisma } from "@prisma/client";
+
+export interface User{
+  id: string;
+  username: string;
+  name: string;
+  lastName?: string | null;
+  profileImageUrl?: string | null;
+  bio?: string | null;
+  email: string;
+  gender?: Gender;
+}
+
+export type CreateUserDto = Prisma.UserCreateInput;
+export type UpdateUserDto = Prisma.UserUpdateInput;
