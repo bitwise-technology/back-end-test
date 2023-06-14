2c794e70329b0818e493904aef2c4e76b9f4afdf
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-13 22:52:24 -0300

    feat(routes): passing get user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,18 @@
-import { Router } from "express";
-import { UserController } from "../controllers/userControllers/user.controller";
+import { Router } from 'express';
+import { UserController } from '../controllers/userControllers/user.controller';
 
 const userController = new UserController();
 
 const userRoutes = Router();
-userRoutes.post("/", userController.createUser.bind(userController));
-userRoutes.post("/github", userController.createGitHubUser.bind(userController));
-userRoutes.put("/update/:username", userController.updateUser.bind(userController));
+userRoutes.post('/', userController.createUser.bind(userController));
+userRoutes.post(
+  '/github',
+  userController.createGitHubUser.bind(userController),
+);
+userRoutes.put(
+  '/update/:username',
+  userController.updateUser.bind(userController),
+);
+userRoutes.get('/:username', userController.findUser.bind(userController));
 
-export {userRoutes};
+export { userRoutes };
