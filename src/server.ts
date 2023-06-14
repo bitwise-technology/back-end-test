9472cab27c60cf109159b3560862916aa4890850
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-14 08:40:07 -0300

    docs(swagger): add swagger to the project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { routes } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+
+
+const swaggerFile = readFileSync('./swagger.json', 'utf8');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(JSON.parse(swaggerFile)));
 
 app.listen(3333, () => console.log("Server is running in port 3333"));
