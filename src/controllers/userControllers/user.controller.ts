d61f33dcb64c29f4dba9736ec122404cf229208a
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-13 21:20:56 -0300

    feat(controller): create user update method

--- src/controllers/userControllers/user.controller.ts
+++ src/controllers/userControllers/user.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import { PrismaClient, User } from '@prisma/client';
+import { Prisma, PrismaClient, User } from '@prisma/client';
 import { UserUseCase } from '../../usecases/usersUseCase/userUseCase';
-import { CreateUserDto } from '../../models/user.model';
+import { CreateUserDto, UpdateUserDto } from '../../models/user.model';
 import { StatusCodes } from 'http-status-codes';
 import gitHubApi from '../../services/gitHubApi';
 
-export const Prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 export class UserController {
   private readonly userUseCase: UserUseCase;
@@ -35,11 +35,15 @@ export class UserController {
     }
 
     try {
-      const existingUser = await Prisma.user.findUnique({
+      const existingUsername = await prisma.user.findUnique({
         where: { username: createUserDto.username },
       });
 
-      if (existingUser) {
+      const existingEmail = await prisma.user.findUnique({
+        where: { email: createUserDto.email },
+      });
+
+      if (existingUsername || existingEmail) {
         return res
           .status(StatusCodes.BAD_REQUEST)
           .json({ error: 'User already exists!' });
@@ -48,7 +52,6 @@ export class UserController {
       const user: User = await this.userUseCase.createUser(createUserDto);
       res.status(StatusCodes.CREATED).json(user);
     } catch (error) {
-      console.error(error);
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         message: 'An error occurred while creating the user',
       });
@@ -58,7 +61,7 @@ export class UserController {
   public async createGitHubUser(req: Request, res: Response) {
     const { username } = req.body;
 
-    const existingUser = await Prisma.user.findUnique({
+    const existingUser = await prisma.user.findUnique({
       where: { username: username },
     });
 
@@ -69,13 +72,13 @@ export class UserController {
     }
 
     try {
-      const response = await gitHubApi.get(`/users/${username}`);
+      const response = await gitHubApi.get(`/${username}`);
 
       const { data } = response;
 
       const user: User = await this.userUseCase.createUser({
         username: data.login,
-        name: data.name,
+        name: data.name ? data.name : 'User does not have name',
         email: data.email ? data.email : `${username}@emailnotfound.com`,
         profileImageUrl: data.avatar_url,
         bio: data.bio,
@@ -83,16 +86,68 @@ export class UserController {
 
       res.status(StatusCodes.CREATED).json(user);
     } catch (error) {
-      if (error instanceof Error) {
-        if (error.response && error.response.status === 404) {
-          return res.status(StatusCodes.BAD_REQUEST).json({
-            message: 'User not found on GitHub.',
-          });
-        }
+      if (error instanceof Error && error.response?.status === 404) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: 'User not found on GitHub.' });
       }
 
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         message: 'An error occurred while creating the GitHub user',
+      });
+    }
+  }
+
+  public async updateUser(req: Request, res: Response) {
+    const { username } = req.params;
+    const updateUserData = req.body;
+    const validationRules = [
+      { field: 'username', required: true },
+      { field: 'name', required: true },
+      { field: 'email', required: true },
+    ];
+
+    for (const { field, required } of validationRules) {
+      const value = updateUserData[field];
+
+      if (required && !value) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: `Missing required field: ${field}`,
+        });
+      }
+    }
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: { username },
+      });
+
+      if (!user) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: 'User not found!' });
+      }
+
+      const conflictingUser = await prisma.user.findUnique({
+        where: { email: updateUserData.email },
+      });
+
+      if (conflictingUser && updateUserData.email !== user.email) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            error: 'Email address already associated with another account.',
+          });
+      }
+
+      const updatedUser = await this.userUseCase.updateUser(
+        updateUserData,
+        username,
+      );
+      res.status(StatusCodes.CREATED).json(updatedUser);
+    } catch (error) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: 'An error occurred while updating the user',
       });
     }
   }
