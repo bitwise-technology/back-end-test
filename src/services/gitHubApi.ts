b39eccce1ac12f9b080d62450f20cf56bf987e19
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-14 08:40:48 -0300

    style(service): update to be on conform to the eslint

--- src/services/gitHubApi.ts
+++ src/services/gitHubApi.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.github.com/users',
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: 'https://api.github.com/users',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
