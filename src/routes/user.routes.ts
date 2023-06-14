fefb1d810f2b5ce9352a0fbcb7a6b37f7396f67c
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-13 21:22:07 -0300

    feat(routes): create user update route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,5 +6,6 @@ const userController = new UserController();
 const userRoutes = Router();
 userRoutes.post("/", userController.createUser.bind(userController));
 userRoutes.post("/github", userController.createGitHubUser.bind(userController));
+userRoutes.put("/update/:username", userController.updateUser.bind(userController));
 
 export {userRoutes};
